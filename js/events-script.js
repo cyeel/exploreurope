0998a669de13466867fdae8d0f06db4b220e8110
cyeel <annisa.elcye@gmail.com>
2025-11-09 22:29:40 +0700

    Delete js/events-script.js

--- js/events-script.js
+++
@@ -1,9 +0,0 @@
-const popup = document.getElementById("popup");
-    const closeBtn = document.querySelector(".close");
-    const festivalDay = document.querySelector(".festival");
-
-    festivalDay.onclick = () => popup.style.display = "flex";
-    closeBtn.onclick = () => popup.style.display = "none";
-    popup.onclick = (e) => {
-      if (e.target === popup) popup.style.display = "none";
-    };
