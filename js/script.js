0cd92059c58e3eafa6650a2eab63e4763fb9c2da
cyeel <annisa.elcye@gmail.com>
2025-11-11 07:09:39 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const swiper = new Swiper('.swiper', {
+// --- Video Swiper ---
+const videoSwiper = new Swiper('.video-swiper', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.videos .swiper-button-next',
+    prevEl: '.videos .swiper-button-prev',
+  },
+});
+
+// --- Destination Swiper ---
+const destinationSwiper = new Swiper('.destination-swiper', {
   slidesPerView: 3,
   spaceBetween: 20,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.destinations .swiper-button-next',
+    prevEl: '.destinations .swiper-button-prev',
   },
   breakpoints: {
     768: { slidesPerView: 3 },
